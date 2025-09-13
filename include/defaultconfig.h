0954379d51503415bda3f8d5c810e18ea6bb77e3
Louis du Verdier <LouisDuVerdier@users.noreply.github.com>
2025-09-13 20:10:22 +0200

    README: use Material color scheme recommendations for default config + update example of visualization

--- include/defaultconfig.h
+++ include/defaultconfig.h
@@ -5,21 +5,21 @@
 static constexpr std::string_view DEFAULT_CONFIG_STR = R"EOF(rules:
 - description: IPv4
   regex: \b((?:25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})(?:\.(?:25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})){3})\b
-  color: f#FF4DFF
+  color: f#E666FB
 
 - description: MAC address
   regex: \b([0-9A-Fa-f]{2}(?::[0-9A-Fa-f]{2}){5})\b
-  color: f#FF4DFF
+  color: f#E666FB
 
 - description: Error
   regex: (?i)\b(password|abnormal(ly)?|down|los(t|ing)|err(ors?)?|(den(y|ies|ied)?)|reject(ing|ed)?|drop(ped|s)?|(err\-)?disabled?|(timed?\-?out)|fail(s|ed|iure)?|disconnect(ed)?|unreachable|invalid|bad|notconnect|unusable|block(ed|ing)?|blk|inaccessible|wrong|collisions?|unsynchronized|mismatch|runts)\b
-  color: f#FF6060
+  color: f#F44336
 
 - description: Warning
   regex: (?i)\b(warn(ing)?s?|no(pe)?|exit(ed)?|reset(t?ing)?|discard(ed|ing)?|filter(ed|ing)?|stop(p(ed|ing))?|never|bad|can((')?t|not))\b
-  color: f#DEDC12
+  color: f#CDDC39
 
 - description: Success
   regex: (?i)\b(ye(s|ah?|p)?|start(ed|ing)?|running|good|up|ok(ay)?|permit(ed|s)?|accept(s|ed)?|enabled?|online|succe((ss(ful|fully)?)|ed(ed)?)?|connect(ed)?|reachable|valid|forwarding|synchronized)\b
-  color: f#18ED93
+  color: f#33D375
 )EOF";
